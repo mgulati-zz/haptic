9d5c1a333d7e466bc3bede63e04437b01b16a9c7
Lesia Nalepa <lesia.nalepa@gmail.com>
2015-03-01 23:36:18 -0500

    simple form chrome app

--- Form/main.js
+++ Form/main.js
@@ -1,45 +1,10 @@
-function changeVolume() {
-	console.log('slider UI ' + $('.single-slider').val());
-	$('audio')[0].volume = $('.single-slider').val() * 0.01;
-	var volume = $('audio')[0].volume;
 
-	var color = "#000000";
-	if (volume == 1 ) color = "#ffffff";
-	if (volume < 0.75 ) color = "#0000ff";
-	if (volume < 0.50 ) color = "#00ff00";
-	if (volume < 0.25 ) color = "#ff0000";
-	if (volume == 0 ) color = "#000000";
-	changeColor(color);
-}
-
-function toggleState() {
-	if ($(".fa").hasClass('fa-play')) {
-		$(".fa").removeClass('fa-play');
-		$(".fa").addClass('fa-pause');
-		$('audio')[0].play();
-	}
-	else {
-		$(".fa").removeClass('fa-pause');
-		$(".fa").addClass('fa-play');
-		$('audio')[0].pause();
-	}
+function logIn() {
+	console.log('here');
 }
 
 $(document).ready(function(){
-  $('.single-slider').jRange({
-      from: 0,
-      to: 100,
-      step: 1,
-      format: '%s',
-      width: 500,
-      showLabels: true,
-      onstatechange: changeVolume
-  });
-  $('audio')[0].volume = $('.single-slider').val() * 0.01;
-
-  $(".fa").click(function(){
-		toggleState();
-	})
+  
 });
 
 var connectionId;
@@ -84,10 +49,6 @@ function onLineReceived(str) {
 	str.split(',');
 	var touch = parseInt(str[0]);
 	var pos = parseInt(str[1]);
-
-	var volume = pos * 0.1;
-	$('.single-slider').val(volume);
-	$('audio')[0].volume = volume;
 }
 
 /* Interprets an ArrayBuffer as UTF-8 encoded string data. */
