fe38376dee279145b6384b5cea3c60811b92eb17
Mayank Gulati <mayank_gulati@hotmail.com>
2015-03-02 07:01:48 -0500

    button behaviour on login

--- Form/login.js
+++ Form/login.js
@@ -1,20 +1,61 @@
+var loginBtn;
+
+var button = new Button(function(btn) {
+  if (!loginBtn.hasClass('disabled') && btn.currentColor != "#ff0000") {
+    
+    if (btn.touch == 1 || $(".login-button:hover").length > 0) {
+      loginBtn.addClass("hovered");
+      btn.changeColor('#0000ff');
+    }
+    else {
+      loginBtn.removeClass("hovered");
+      btn.changeColor('#00ff00');
+    }
+
+    if (btn.position < 800 && btn.touch == 1 && btn.desiredPosition == 1000) {
+      loginBtn.addClass("clicking");
+    } else {
+      loginBtn.removeClass("clicking");
+    }
+
+    if (btn.position < 500 && btn.touch == 1 && btn.desiredPosition == 1000) {
+      logIn();
+    }
+
+  } else {
+    btn.changeColor('#000000');
+    btn.sendTarget(0);
+  }
+
+});
+button.disableSlide();
+
 function logIn() {
-  if (!$(".login-button").hasClass('disabled')) {
+  if (!loginBtn.hasClass('disabled')) {
     $(".error-message").html("");
     var name = $("#name").val();
     var password = $("#password").val();
     if (name == '' || password == '') {
       $(".error-message").html("Please fill in all information");
       console.log('Please fill in all information');
+      loginBtn.addClass('disabled');
+      button.changeColor('#000000');
+      button.sendTarget(0);
     } else {
-      $(".login-button").html("LOGGING IN...");
+      loginBtn.html("LOGGING IN...");
+      button.changeColor('#ffffff');
+      button.sendTarget(60);
       window.setTimeout(function (){
         if (password === 'password') {
           $(".form").css("display","none");
           $(".success").css("display","block");
+          button.changeColor('#00ff00');
+          button.sendTarget(30);
         } else {
           $(".error-message").html("Password is incorrect");
-          $(".login-button").html("LOG IN");
+          loginBtn.html("LOG IN");
+          button.changeColor('#ff0000');
+          button.sendTarget(100);
         }
       }, 1000);
     }
@@ -22,83 +63,22 @@ function logIn() {
 }
 
 $(document).ready(function(){
+
+  loginBtn = $(".login-button");
+  loginBtn.click(logIn);
+
   $("input").keyup(function(){
     var name = $("#name").val();
     var password = $("#password").val();
     if (name != '' && password != '') {
-      $(".login-button").removeClass('disabled');
+      loginBtn.removeClass('disabled');
+      button.changeColor('#00ff00');
+      button.sendTarget(100);
+    } else {
+      loginBtn.addClass('disabled');
+      button.changeColor('#000000');
+      button.sendTarget(0);
     }
   })
+
 });
-
-var connectionId;
-
-function writeSerial(str) {
-	chrome.serial.send(connectionId, convertStringToArrayBuffer(str), function(){ console.log("write "+ str) });
-}
-
-function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
-function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
-function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
-function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
-
-function changeColor(hex) {
-	R = hexToR(hex);
-	G = hexToG(hex);
-	B = hexToB(hex);
-
-	writeSerial("R" + R + "\n");
-	writeSerial("G" + G + "\n");
-	writeSerial("B" + B + "\n");
-}
-
-function onConnect(connectionInfo) {
-	connectionId = connectionInfo.connectionId;
-}
-chrome.serial.connect("/dev/tty.usbmodem1421", {bitrate: 9600}, onConnect);
-
-// Convert string to ArrayBuffer
-function convertStringToArrayBuffer(str) {
-  var buf=new ArrayBuffer(str.length);
-  var bufView=new Uint8Array(buf);
-  for (var i=0; i<str.length; i++) {
-    bufView[i]=str.charCodeAt(i);
-  }
-  return buf;
-}
-
-var stringReceived = '';
-
-function onLineReceived(str) {
-	strs = str.split(',');
-	var touch = parseInt(strs[0]);
-	var pos = parseInt(strs[1]);
-}
-
-/* Interprets an ArrayBuffer as UTF-8 encoded string data. */
-function convertArrayBufferToString(buf) {
-  var bufView = new Uint8Array(buf);
-  var encodedString = String.fromCharCode.apply(null, bufView);
-  return encodedString;
-};
-
-function onReceiveCallback(info) {
-  if (info.data) {
-    var str = convertArrayBufferToString(info.data);
-    if (str.charAt(str.length-1) === '\n') {
-      stringReceived += str.substring(0, str.length-1);
-      stringsRecieved = stringReceived.split('\n');
-      stringsRecieved.forEach(function(strRec) {
-      	onLineReceived(strRec);
-      })
-      stringReceived = '';
-    } else {
-      stringReceived += str;
-    }
-  }
-};
-
-chrome.serial.onReceive.addListener(onReceiveCallback);
-
-
-
