760b5ae8e0917332db8d585c5297efa12beb970b
Adam Wootton <ajwootto@uwaterloo.ca>
2015-03-14 13:24:46 -0400

    fix id thing

--- libraries/buttons.js
+++ libraries/buttons.js
@@ -7,14 +7,14 @@ function Buttons (arduino) {
   function onLineReceived(str) {
     var strs = str.split(',');
     var id = parseInt(strs[0]);
-    if (buttons.id.touch == parseInt(strs[1]) && buttons.id.position == parseInt(strs[2])) return;
+    if (buttons[id].touch == parseInt(strs[1]) && buttons[id].position == parseInt(strs[2])) return;
     var touch = parseInt(strs[1]);
     var position = parseInt(strs[2]);
     var desiredPosition = parseInt(strs[3]);
-    buttons.id.updateValues(touch,position,desiredPosition);
+    buttons[id].updateValues(touch,position,desiredPosition);
   }
 
   function newButton(id, onUpdate) {
-    buttons.id = new Button(id, arduino, onUpdate);
+    buttons[id] = new Button(id, arduino, onUpdate);
   }
 }
