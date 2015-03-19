29cbaa6931fa034b5f09238b23147ad5ac9af0df
Adam Wootton <ajwootto@uwaterloo.ca>
2015-03-19 12:10:48 -0400

    declare proper global variable

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  grid = new Grid();
+  window.grid = new Grid();
   for (var i = 0; i < 4; i++) {
     grid.newButton(i, function(button) {
       $slider = $(".slide:eq(" + button.id +")");
