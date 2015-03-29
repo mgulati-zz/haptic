756d8eab8ddf2deec5b982e9b4d32b97edc9c8bb
Mayank Gulati <mayank_gulati@hotmail.com>
2015-03-29 16:28:08 -0400

    updated for a 3x3

--- chrome/Prototype_3/DevPage/devPage.js
+++ chrome/Prototype_3/DevPage/devPage.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
   
   var colors = ["#800000","FF281E","#FF0000","#FF0A00","#FF8C00","#FFA500","#FFD700","FFE600","#FFFF00","#9400D3","#8B008B","#4B0082","#483D8B","#6A5ACD","#7B68EE","#7FFF00","#008000","#006400","#008B8B","#008080","#00CED1","#4682B4","#87CEEB","#87CEFA","#00BFFF","#1E90FF","#6495ED","#4169E1","#0000FF","#0000CD"]
-  var x = 2;
-  var z = 2;
+  var x = 3;
+  var z = 3;
   var connected = false;
 
   //debuggable gnerator and grid
   generator = new Generator();
-  generator.makeHaptic(x,z,7,4,0.3,1, updateDesired, null, randomColor);
-  generator.setZoom(2.5);
+  generator.makeHaptic(x,z,10,3.5,0.3,0.75, updateDesired, null, randomColor);
+  generator.setZoom(1.5);
 
   grid = new Grid();
   for (var i = 0; i < 9; i++) {
     grid.newButton(i, function(button) {
       var coordinates = grid.indexLookup(button.id);
-      if (coordinates[0] > 1 || coordinates[1] > 1) return; //for 2x2 grid right now
       generator.setPos(coordinates[0], coordinates[1], button.position / 1000, false);
       generator.setDesiredPos(coordinates[0], coordinates[1], button.desiredPosition / 1000, false);
       if (connected == false) {
@@ -41,12 +40,12 @@ $(document).ready(function() {
 
   function resetAll() {
     //colors
-    for (var x = 0; x < 2; x++) {
-      for (var z = 0; z < 2; z++) {
-        updateDesired(x,z,0);
-        generator.setLEDColor(x,z,'#ffffff');
-        grid.updateColor(grid.coordinateLookup(x,z),'#ffffff');
-        generator.setDesiredPos(x,z,0,true);
+    for (var x_i = 0; x_i < x; x_i++) {
+      for (var z_i = 0; z_i < z; z_i++) {
+        generator.setLEDColor(x_i,z_i,'#ffffff');
+        grid.updateColor(grid.coordinateLookup(x_i,z_i),'#ffffff');
+        generator.setDesiredPos(x_i,z_i,0,true);
+        updateDesired(x_i,z_i,0);
       }
     }
     $('.scene').css('transition','transform 0.5s ease-in-out');
