5f33e06da693f81dfafac84fd19b6e6b19a2e05f
Mayank Gulati <mayank_gulati@hotmail.com>
2015-03-30 00:16:58 -0400

    spacing

--- chrome/Prototype_3/background.js
+++ chrome/Prototype_3/background.js
@@ -2,8 +2,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('DevPage/devPage.html', {
     'id': 'dev',
     'bounds': {
-      "width" :   window.screen.availWidth,
-      "height":   window.screen.availHeight
+      "width" : window.screen.availWidth,
+      "height": window.screen.availHeight
     }, "state": "fullscreen"
   });
 });
