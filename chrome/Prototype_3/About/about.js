4b784aaf3e3f1d863ce0e21688f142e03693e34b
Lesia Nalepa <lesia.nalepa@gmail.com>
2015-03-30 02:31:28 -0400

    animations

--- chrome/Prototype_3/About/about.js
+++ chrome/Prototype_3/About/about.js
@@ -1,7 +1,14 @@
+threeD = new Generator();
+grid = new Grid();
+
 $(function() {
-  $('img').hover(
-	  function() {
+  $('.tile').hover(function() {
+	    $(this).children('.caption').css('bottom', '54px');
 	  }, function() {
+	    $(this).children('.caption').css('bottom', '0px');
 	  }
-	);
+	).click(function() {
+		$(this).parent().children('.tile').css('top','80%');
+		$(this).css({'top':'100px', 'left':'100px'});
+	});
 })
